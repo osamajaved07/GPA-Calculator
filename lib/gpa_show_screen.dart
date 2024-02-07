78799be9f06f289e2875417a76d0dca4fcf87798
Osama Javed <www.om173116@gmail.com>
2024-02-07 12:48:56 +0500

    This is Updated changes

--- lib/gpa_show_screen.dart
+++ lib/gpa_show_screen.dart
@@ -1,7 +1,6 @@
-// ignore_for_file: camel_case_types, prefer_const_constructors
+// ignore_for_file: camel_case_types, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
-
 
 class GPA_Show_Screen extends StatelessWidget {
   final double gpa;
@@ -16,17 +15,34 @@ class GPA_Show_Screen extends StatelessWidget {
         backgroundColor: Color.fromARGB(255, 0, 128, 128),
       ),
       body: Center(
+        
         child: Column(
           
+          // mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            SizedBox(height: 20,),
+
+            
             Text(
               'Your GPA is:',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
             ),
             SizedBox(height: 16),
             Text(
               gpa.toStringAsFixed(2), // Format GPA to display only two decimal places
-              style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 255, 255, 255)),
+              style: TextStyle(
+                fontSize: 48,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                fontFamily: 'Roboto', // Example font family
+                shadows: [
+                  Shadow(
+                    color: Colors.black,
+                    blurRadius: 4,
+                    offset: Offset(2, 2),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
