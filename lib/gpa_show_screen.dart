806519e4eabaae4aaee78e969e4a2439c377dc74
Osama Javed <www.om173116@gmail.com>
2024-02-09 11:28:51 +0500

    This is final app

--- lib/gpa_show_screen.dart
+++ lib/gpa_show_screen.dart
@@ -17,81 +17,98 @@ class GPA_Show_Screen extends StatelessWidget {
   
       backgroundColor: Color.fromARGB(255, 0, 128, 128),
   
-      appBar: AppBar(
+      
   
-        backgroundColor: Color.fromARGB(255, 0, 128, 128),
-  
-      ),
-  
-      body: Center(
-  
-        child: Column(
-  
-          // mainAxisAlignment: MainAxisAlignment.center,
-  
-          children: [
-  
-            SizedBox(
-  
-              height: 20,
-  
-            ),
-  
-            Text(
-  
-              'Your GPA is:',
-  
-              style: TextStyle(
-  
-                  fontSize: 24,
-  
-                  fontWeight: FontWeight.bold,
-  
-                  color: Colors.white),
-  
-            ),
-  
-            SizedBox(height: 16),
-  
-            Text(
-  
-              gpa.toStringAsFixed(
-  
-                  2), // Format GPA to display only two decimal places
-  
-              style: TextStyle(
-  
-                fontSize: 48,
-  
-  
-                fontWeight: FontWeight.bold,
-  
-                color: Colors.white,
-  
-                fontFamily: 'Roboto', // Example font family
-  
-                shadows: [
-  
-                  Shadow(
-  
-                    color: Colors.black,
-  
-                    blurRadius: 4,
-  
-                    offset: Offset(2, 2),
-  
-                  ),
-  
-                ],
-  
+      body: SafeArea(
+        minimum: EdgeInsets.only(top: 100),
+        child: Center(
+          
+          child: Column(
+          
+            // mainAxisAlignment: MainAxisAlignment.center,
+          
+            children: [
+          
+              SizedBox(
+          
+                height: 20,
+          
               ),
-  
-            ),
-  
-          ],
-  
+          
+              Text(
+                
+                'Your GPA is:',
+                
+                style: TextStyle(
+                
+                    fontSize: 24,
+                
+                    fontWeight: FontWeight.bold,
+                
+                    color: Colors.white),
+                
+              ),
+          
+              SizedBox(height: 16),
+          
+              Text(
+                
+                gpa.toStringAsFixed(
+                
+                    2), // Format GPA to display only two decimal places
+                
+                style: TextStyle(
+                
+                  fontSize: 48,
+                
+                
+                  fontWeight: FontWeight.bold,
+                
+                  color: Colors.white,
+                
+                  fontFamily: 'Roboto', // Example font family
+                
+                  shadows: [
+                
+                    Shadow(
+                
+                      color: Color.fromARGB(255, 249, 0, 0),
+                
+                      blurRadius: 4,
+                
+                      offset: Offset(2, 2),
+                
+                    ),
+                
+                  ],
+                
+                ),
+                
+              ),
+        
+              SizedBox(height: 150),
+        
+              ElevatedButton(
+                style: ButtonStyle(
+        
+                    backgroundColor: MaterialStateProperty.all(
+        
+                        Color.fromARGB(255, 54, 69, 79)),
+        
+                  ),
+                  
+                  onPressed: (){
+                    Navigator.pop(context);
+        
+              }, child: Text("Back", style: TextStyle(
+                color: Colors.white, fontSize: 16 
+              ),))
+          
+            ],
+          
+          ),
+          
         ),
-  
       ),
 
 
